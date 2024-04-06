e357eb3b669b83b2dace2d74a3753984dfc34a9a
Leak1 <2024028sun@aupp.edu.kh>
2024-04-06 22:04:23 +0700

    Khingleak SUN

--- exercise-5.py
+++ exercise-5.py
@@ -4,5 +4,6 @@ def reverse_ascending(numbers):
             if numbers[i] < numbers[i + 1]:
                 numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
     return numbers
+
 number = [2,5,3,6,7,8,9,10,11,12]
 print(reverse_ascending(number))
