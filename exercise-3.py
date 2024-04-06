b6fe1fab8c54d482f06d97edb35c70ae0975ae56
Taiseng045 <2024045lai@aupp.edu.kh>
2024-04-06 21:54:34 +0700

    Taiseng LAI

--- exercise-3.py
+++ exercise-3.py
@@ -1,9 +1,11 @@
-def remove_all_after(numbers, n):
+def remove_all_after(number, n):
     list= []
-    for i in numbers:
-        list.append(i)
-        if i == n:
+    i=0
+    while i< len(number):
+        list.append(number[i])
+        if number[i] == n:
             break
+        i+=1
     return list
 number = [2,5,3,6,7,8,9,10,11,12]
 print(remove_all_after(number, 9))
